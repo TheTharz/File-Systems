ccdf9be7105abd84bba0a2cb765c885058c964a7
TheTharz <tharinduimalka915@gmail.com>
2025-04-22 05:15:40 +0000

    Add change_the_permissions_mode functionality to manage file permissions and writing

---
+++ low_level_io/change_the_permissions_mode.c
@@ -0,0 +1,38 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <stdio.h>
+
+int main(void){
+  int fd1 = open("./doc3.txt",O_RDWR|O_CREAT|O_TRUNC,S_IRUSR|S_IWUSR|S_IXUSR);
+  if(fd1==-1){
+    perror("./doc3.txt");
+    printf("Error while opening the file doc3 %d",errno);
+    exit(0);
+  }else{
+    close(fd1);
+  }
+
+  chmod("./doc3.txt",O_RDONLY);
+
+  int fd2 = open("./doc3.txt",O_RDONLY,S_IRUSR);
+  if(fd2==-1){
+    perror("./doc3.txt");
+    printf("Error while opening the file doc3 %d",errno);
+    exit(0);
+  }else{
+    char data[5]="hello";
+    int bytes_written = write(fd1,data,5);
+
+    if(bytes_written==-1){
+      perror("./doc3.txt");
+      printf("Error while writing the file doc3 %d",errno);
+      exit(0);
+    }else{
+      close(fd2);
+    }
+  }
+  
+}
