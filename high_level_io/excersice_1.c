d190371033954aa5f2f1a56ff8d23c4587fc6ce8
TheTharz <tharinduimalka915@gmail.com>
2025-04-22 06:54:57 +0000

    Implement file writing functionality to capture user name and age with error handling

---
+++ high_level_io/excersice_1.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+
+int main(void){
+  int size;
+  printf("What is the size of your name : ");
+  scanf("%d",&size);
+
+  char name[size];
+  printf("What is your name : ");
+  scanf("%s",name);
+
+  char age[2];
+  printf("What is your age");
+  scanf("%s",age);
+
+  FILE * fp1 = fopen("./doc1.txt","w+");
+
+  if(fp1==NULL){
+    printf("Doc1.txt could not be opened");
+    perror("./doc1.txt");
+    printf("Error number : %d",errno);
+    exit(0);
+  }
+  else{
+    printf("Doc1.txt opened succesfully");
+
+    int written_bytes = fprintf(fp1,"My name is %s and my age is %s",name,age);
+    if(written_bytes == -1){
+      printf("Writting has been failed to file");
+      perror("./doc1.txt");
+      printf("Error number : %d",errno);
+      exit(0);
+    }else{
+      printf("Bytes were written succesfully");
+      fclose(fp1);
+    }
+  }
+
+
+
+
+  return 0;
+}
